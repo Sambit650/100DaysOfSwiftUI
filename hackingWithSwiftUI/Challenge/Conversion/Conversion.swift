612f85bd9858cc8a7e49e11c33d26f9df14505df
Sambit650 <sambit650@gmail.com>
2021-05-14 23:37:19 +0530

    added logic for length conversion

--- hackingWithSwiftUI/Challenge/Conversion/Conversion.swift
+++ hackingWithSwiftUI/Challenge/Conversion/Conversion.swift
@@ -14,12 +14,53 @@ struct Conversion: View {
     @State private var inputUnitValue: String = ""
     @State private var inputUnit:Int = 0
     @State private var outputUnit:Int = 1
-    var result: String = ""
-    var conversionList = ["meters", "KM", "feet", "yard", "miles"]
+    var conversionList = ["meters", "KM", "feet", "yards", "miles"]
+    
+    enum Unit: String, CaseIterable {
+        case meters
+        case km
+        case feet
+        case yards
+        case miles
+        
+        var unitLength: UnitLength {
+            switch self {
+            case .meters: return UnitLength.meters
+            case .km: return UnitLength.kilometers
+            case .feet: return UnitLength.feet
+            case .yards: return UnitLength.yards
+            case .miles: return UnitLength.miles
+            }
+        }
+    }
+    
+    let lengthUnits = Unit
+        .allCases
+        .compactMap { $0.rawValue }
+    
+    var output: Double {
+        guard let input = Double(self.inputUnitValue) else {
+            return 0
+        }
+        guard let inputUnit = Unit(rawValue: lengthUnits[self.inputUnit]) else {
+            return 0
+        }
+        guard let outputUnit = Unit(rawValue: lengthUnits[self.outputUnit]) else {
+            return 0
+        }
+        return Measurement(value: input, unit: inputUnit.unitLength).converted(to: outputUnit.unitLength).value
+    }
+    
+    // MARK:- Views
     
     var body: some View {
         NavigationView {
             Form {
+                Section {
+                    TextField("Enter \(conversionList[inputUnit])", text: $inputUnitValue)
+                        .keyboardType(.numberPad)
+                }
+                
                 Section(header: Text("Choose the Input unit type.")) {
                     Picker("Select type of conversion", selection: $inputUnit) {
                         ForEach(0 ..< conversionList.count) {
@@ -37,17 +78,11 @@ struct Conversion: View {
                 }
                 
                 Section {
-                    TextField("Enter \(conversionList[inputUnit])", text: $inputUnitValue)
-                }
-                
-                Section {
-                    Text("\(inputUnitValue) \(conversionList[inputUnit]) is equal to \(result) \(conversionList[outputUnit])")
+                    Text("\(inputUnitValue.isEmpty ? "0" : inputUnitValue) \(conversionList[inputUnit]) is equal to \(output) \(conversionList[outputUnit])")
                 }
             }
             .navigationBarTitle("Conversion", displayMode: .automatic)
         }
-        
-        
     }
 }
 
