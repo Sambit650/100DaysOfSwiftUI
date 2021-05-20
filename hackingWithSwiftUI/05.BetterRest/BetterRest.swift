33d586e948b969b409fa449b8609a0ce174da2e8
sambit650 <sambit650@gmail.com>
2021-05-20 11:32:11 +0530

    added BetterRest group

---
+++ hackingWithSwiftUI/05.BetterRest/BetterRest.swift
@@ -0,0 +1,20 @@
+//
+//  BetterRest.swift
+//  hackingWithSwiftUI
+//
+//  Created by Sambit Das on 20/05/21.
+//
+
+import SwiftUI
+
+struct BetterRest: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct BetterRest_Previews: PreviewProvider {
+    static var previews: some View {
+        BetterRest()
+    }
+}
