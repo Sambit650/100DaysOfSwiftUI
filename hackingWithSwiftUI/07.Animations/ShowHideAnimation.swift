1f398ee5662eea38887ac34d65b1effac54fbf71
sambit650 <sambit650@gmail.com>
2021-05-31 00:32:22 +0530

    custom transition ViewModifier

--- hackingWithSwiftUI/07.Animations/ShowHideAnimation.swift
+++ hackingWithSwiftUI/07.Animations/ShowHideAnimation.swift
@@ -7,6 +7,25 @@
 
 import SwiftUI
 
+// MARK:- Custom transitions
+
+struct CornerRotationModifier: ViewModifier {
+    let amount: Double
+    let anchor: UnitPoint
+    
+    func body(content: Content) -> some View {
+        content.rotationEffect(.degrees(amount), anchor: anchor)
+            .clipped()
+    }
+}
+
+extension AnyTransition {
+    static var pivot: AnyTransition {
+        .modifier(active: CornerRotationModifier(amount: -90, anchor: .topTrailing),
+                  identity: CornerRotationModifier(amount: 0, anchor: .topTrailing))
+    }
+}
+
 struct ShowHideAnimation: View {
     
     // MARK :- Properties
@@ -27,7 +46,7 @@ struct ShowHideAnimation: View {
                 Rectangle()
                     .fill(Color.red)
                     .frame(width: 200, height: 200, alignment: .center)
-                    .transition(.asymmetric(insertion: .scale, removal: .opacity))
+                    .transition(.pivot)
             }
         }
     }
