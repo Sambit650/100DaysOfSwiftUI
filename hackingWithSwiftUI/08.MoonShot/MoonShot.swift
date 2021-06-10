702c8b51d210fde0930618060463c000baf2b5a0
sambit650 <sambit650@gmail.com>
2021-06-10 23:32:30 +0530

    added assets for moonshot

---
+++ hackingWithSwiftUI/08.MoonShot/MoonShot.swift
@@ -0,0 +1,20 @@
+//
+//  MoonShot.swift
+//  hackingWithSwiftUI
+//
+//  Created by Sambit Das on 10/06/21.
+//
+
+import SwiftUI
+
+struct MoonShot: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MoonShot_Previews: PreviewProvider {
+    static var previews: some View {
+        MoonShot()
+    }
+}
