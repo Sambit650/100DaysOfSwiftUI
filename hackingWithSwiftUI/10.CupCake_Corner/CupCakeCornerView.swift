ea46b2285ac510e8bbf78bc893c4048fa5b46582
sambit650 <sambit650@gmail.com>
2021-06-28 20:24:42 +0530

    added file

---
+++ hackingWithSwiftUI/10.CupCake_Corner/CupCakeCornerView.swift
@@ -0,0 +1,20 @@
+//
+//  CupCakeCornerView.swift
+//  hackingWithSwiftUI
+//
+//  Created by Sambit Das on 28/06/21.
+//
+
+import SwiftUI
+
+struct CupCakeCornerView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CupCakeCornerView_Previews: PreviewProvider {
+    static var previews: some View {
+        CupCakeCornerView()
+    }
+}
